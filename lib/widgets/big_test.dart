0a33ff222c6d20666e2e877e19154fc71e35ccdf
Sabiha <ssabiha104@gmail.com>
2024-10-29 03:36:48 -0700

    Initial commit

---
+++ lib/widgets/big_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+
+class BigText extends StatelessWidget {
+  const BigText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(""
+        "");
+  }
+}
