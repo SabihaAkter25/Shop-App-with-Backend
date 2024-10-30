7a77f46f23a7d2c3d4f014ccc84455f6901ed30f
Sabiha <ssabiha104@gmail.com>
2024-10-29 22:22:38 -0700

    Initial commit

--- lib/widgets/big_test.dart
+++ lib/widgets/big_test.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class BigText extends StatelessWidget {
-  const BigText({super.key});
+  Color? color;
+  final String text;
+  double size;
+  TextOverflow overflow;
+
+   BigText({super.key,  this.color=Colors.tealAccent, required this.text,
+   this.overflow=TextOverflow.ellipsis,
+     this.size=20
+   });
 
   @override
   Widget build(BuildContext context) {
-    return const Text(""
-        "");
+    return Text(
+        text,
+    overflow: overflow,
+      style: TextStyle(
+        color: color,
+        fontWeight: FontWeight.w400,
+        fontSize: size,
+      ),
+
+    );
   }
 }
