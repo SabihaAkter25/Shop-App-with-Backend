260cbf7ef65e9a972a9eec049f669d516d6a4d30
Sabiha <ssabiha104@gmail.com>
2024-11-02 12:24:15 -0700

    Add pagebody

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -13,8 +13,8 @@ class FoodPageBody extends StatefulWidget {
 class _FoodPageBodyState extends State<FoodPageBody> {
  PageController pageController = PageController(viewportFraction: 0.85);
  var _currPageValue = 0.8;
- double _scaleFactor = 0.8;
- double _height =220;
+ final double _scaleFactor = 0.8;
+ final double _height =220;
 
  @override
   void initState(){
@@ -27,12 +27,13 @@ class _FoodPageBodyState extends State<FoodPageBody> {
  }
 @override
  void dispose(){
+    super.dispose();
    pageController.dispose();
 }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SizedBox(
       height: MediaQuery.sizeOf(context).height*0.35,
       child: PageView.builder(
         controller: pageController,
@@ -44,7 +45,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
     );
   }
   Widget _buildPageItem(int index){
-   Matrix4 matrix = new Matrix4.identity();
+   Matrix4 matrix = Matrix4.identity();
    if(index == _currPageValue.floor()){
      var currScale = 1 - (_currPageValue - index)*(1-_scaleFactor);
      var currTrans = _height*(1-currScale)/2;
@@ -60,7 +61,9 @@ class _FoodPageBodyState extends State<FoodPageBody> {
      var currTrans = _height*(1-currScale)/2;
      matrix = Matrix4.diagonal3Values(1, currScale, 1);
      matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, currTrans, 0);
-
+   }else{
+     var currScale = 0.8;
+     matrix = Matrix4.diagonal3Values(1, currScale, 1)..setTranslationRaw(0, _height*(1-_scaleFactor)/2, 1);
    }
 
     return Transform(
@@ -73,7 +76,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: index.isEven? Colors.tealAccent : Colors.blue,
-            image: DecorationImage(
+            image: const DecorationImage(
               fit: BoxFit.cover,
               image: AssetImage("assets/images/food1.jpg",),
             )
@@ -87,6 +90,22 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(30),
         color: Colors.white,
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0xFFe8e8e8),
+              blurRadius: 5.0,
+              offset: Offset(0, 5)
+            ),
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(-5, 0),
+            ),
+            BoxShadow(
+              color: Colors.white,
+              offset: Offset(5, 0),
+            )
+
+          ]
         ),
           child: Container(
             padding: const EdgeInsets.only(top: 15, left:15,right: 15 ),
@@ -94,7 +113,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 BigText(text: "Chinese Side"),
-                SizedBox(height: 10,),
+                const SizedBox(height: 10,),
                 Row(
                   children: [
                     Wrap(
@@ -114,21 +133,21 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                  Row(
                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                  IconAndText(
+                  const IconAndText(
                     icon: Icons.circle_rounded,
                       text: "Normal",
                        iconColor: Colors.amberAccent,
                   ),
                     SizedBox(width: MediaQuery.sizeOf(context).width*0.02),
       
-                    IconAndText(
+                    const IconAndText(
                       icon: Icons.location_on_sharp,
                       text: "1.7Km",
                       iconColor: Colors.cyan,
                     ),
                     SizedBox(width: MediaQuery.sizeOf(context).width*0.02),
       
-                    IconAndText(
+                    const IconAndText(
                       icon: Icons.access_time,
                       text: "32 min",
                       iconColor: Colors.redAccent,
