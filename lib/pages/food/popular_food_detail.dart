970fcd1458f579031acf7545147072019a8e5ef8
Sabiha <ssabiha104@gmail.com>
2024-11-26 22:51:00 -0800

    Add detail page

--- lib/pages/food/popular_food_detail.dart
+++ lib/pages/food/popular_food_detail.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app_with_backend/utils/dimantions.dart';
+import 'package:shop_app_with_backend/widgets/app_icon.dart';
 
 class PopularFoodDetail extends StatelessWidget {
   const PopularFoodDetail({super.key});
@@ -23,11 +24,15 @@ decoration: const BoxDecoration(
 ),
               ) ),
           Positioned(
+            top: Dimantions.height45,
               left: Dimantions.width20,
               right: Dimantions.width20,
-              child:Row(
+              child:const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              
+              AppIcon(icon: Icons.arrow_back_ios),
+              AppIcon(icon: Icons.shopping_cart_outlined),
+
             ],
           ) )
         ],
