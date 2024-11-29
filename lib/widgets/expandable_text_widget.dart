365fe2ef196afc3d19dc93f87b4f19a645a2a841
Sabiha <ssabiha104@gmail.com>
2024-11-28 22:12:13 -0800

    Add expandable text

--- lib/widgets/expandable_text_widget.dart
+++ lib/widgets/expandable_text_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app_with_backend/utils/dimantions.dart';
 
 class ExpandableTextWidget extends StatefulWidget {
   final String text;
@@ -14,6 +15,16 @@ class _ExpandableTextWidgetState extends State<ExpandableTextWidget> {
  late String secondHalf;
 
  bool hiddenText = true;
+ double textHeight = Dimantions.screenHeight/5.63;
+
+void initSate(){
+  super.initState();
+  if(widget.text.length>textHeight){
+    firstHalf= widget.text.substring(0, textHeight.toInt());
+    secondHalf= widget.text.substring(textHeight.toInt()+1, widget.text.length);
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
