204c1376ea6a56365c0dbda212dfc2e40981588f
Sabiha <ssabiha104@gmail.com>
2024-12-07 00:16:34 -0800

    Add recomended food details

--- lib/pages/food/recomended_food_detail.dart
+++ lib/pages/food/recomended_food_detail.dart
@@ -10,6 +10,15 @@ class RecomendedFoodDetail extends StatelessWidget {
       body: CustomScrollView(
         slivers: [
           SliverAppBar(
+            bottom: PreferredSize(preferredSize: Size.fromHeight(0),
+                child: Container(
+                  color: Colors.white,
+                child: Center(child: Text("Sliver app bar")),
+                width: double.maxFinite,
+                  padding: EdgeInsets.only(bottom: 10),
+                )),
+backgroundColor: Colors.tealAccent.shade700,
+            pinned: true,
             expandedHeight: 300,
             flexibleSpace: FlexibleSpaceBar(
               background: Image.asset("assets/images/food3.jpg",
@@ -17,7 +26,104 @@ class RecomendedFoodDetail extends StatelessWidget {
               fit: BoxFit.cover,),
             ),
           ),
-
+          SliverToBoxAdapter(
+            child: Text("Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts"
+                "Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts"
+                "Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts"
+                "Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts"
+                "Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts"
+                "Food is any substance"
+                " consumed by an organism for"
+                " nutritional support. Food is"
+                " usually of plant, animal, or "
+                "fungal origin and contains "
+                "essential nutrients such as"
+                " carbohydrates, fats, proteins,"
+                " vitamins, or minerals. "
+                "The substance is ingested by an organism "
+                "and assimilated by the organism's cells to provide energy, "
+                "maintain life, or stimulate growth."
+                " Different species of animals have"
+                " different feeding behaviours that"
+                " satisfy the needs of their metabolisms"
+                " and have evolved to fill a specific ecological"
+                " niche within specific geographical contexts")
+          )
         ],
       ),
     );
