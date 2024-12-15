3330cdfc537494327b5a36a386d21aa33d742dd7
Sabiha <ssabiha104@gmail.com>
2024-12-15 13:37:36 -0800

    Add api

---
+++ lib/controller/popular_product_controller.dart
@@ -0,0 +1,23 @@
+
+import 'package:get/get.dart';
+import 'package:shop_app_with_backend/data/api/repository/popular_product_repo.dart';
+
+class PopularProductController extends GetxController {
+  final PopularProductRepo popularProductRepo;
+
+  PopularProductController({required this.popularProductRepo});
+
+  List<dynamic> _popularProductList = [];
+
+  Future<void> getPopularProductList() async {
+    Response response = await popularProductRepo.getPopularProductList();
+
+  }
+}
+
+
+
+
+
+
+
