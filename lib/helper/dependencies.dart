792b25d6b75464996c2a6b7032527f7850f5b208
Sabiha <ssabiha104@gmail.com>
2024-12-16 16:09:49 -0800

    Add api

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,3 +1,14 @@
-Future<void> init()async {
+import 'package:get/get.dart';
+import 'package:shop_app_with_backend/controller/popular_product_controller.dart';
+import 'package:shop_app_with_backend/data/api/api_client.dart';
+import 'package:shop_app_with_backend/data/api/repository/popular_product_repo.dart';
 
+Future<void> init()async {
+  // api client
+Get.lazyPut(()=>ApiClient(appBaseUrl: 'appBaseUrl'));
+//repos
+Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
+//controllers
+Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));
 }
+
