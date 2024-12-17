1624698c74ecae2835f332e3c5516f4701ac02da
Sabiha <ssabiha104@gmail.com>
2024-12-16 16:13:07 -0800

    Add api

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:shop_app_with_backend/pages/food/recomended_food_detail.dart';
-
-void main() {
+import 'helper/dependencies.dart'as dep;
+Future<void> main() async {
+WidgetsFlutterBinding.ensureInitialized();
+  await dep.init();
   runApp(const MyApp());
 }
 
