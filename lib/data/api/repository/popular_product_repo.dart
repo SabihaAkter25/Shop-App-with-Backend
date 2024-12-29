1d9e07ae776ed8fb8787a1922c1312796662fd7a
Sabiha <ssabiha104@gmail.com>
2024-12-29 06:03:25 -0800

    Add api

--- lib/data/api/repository/popular_product_repo.dart
+++ lib/data/api/repository/popular_product_repo.dart
@@ -7,6 +7,6 @@ class PopularProductRepo extends GetxService {
   PopularProductRepo({required this.apiClient});
 
   Future<Response> getPopularProductList() async {
-    return await apiClient.getData("http://mvs.bslmeiyu.com/api/v1/products/popular");
+    return await apiClient.getData("/api/v1/products/popular");
   }
 }
