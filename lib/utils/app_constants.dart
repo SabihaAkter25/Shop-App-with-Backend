96b618982122a7253d1e55732b088cfb97f3a6d4
Sabiha <ssabiha104@gmail.com>
2024-12-31 06:48:22 -0800

    Add api

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -6,4 +6,6 @@ class AppConstant{
   static const String POPULAR_PRODUCTS_URI ="/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCTS_URI ="/api/v1/products/recommended";
 
+  static const String TOKEN ="DBtoken";
+
 }
