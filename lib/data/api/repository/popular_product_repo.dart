a61696f77b4ededb40e1ce76670c9af8144bebb9
Sabiha <ssabiha104@gmail.com>
2024-12-31 06:48:50 -0800

    Add api

--- lib/data/api/repository/popular_product_repo.dart
+++ lib/data/api/repository/popular_product_repo.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:shop_app_with_backend/data/api/api_client.dart';
+import 'package:shop_app_with_backend/utils/app_constants.dart';
 
 class PopularProductRepo extends GetxService {
   final ApiClient apiClient;
@@ -7,6 +8,6 @@ class PopularProductRepo extends GetxService {
   PopularProductRepo({required this.apiClient});
 
   Future<Response> getPopularProductList() async {
-    return await apiClient.getData("/api/v1/products/popular");
+    return await apiClient.getData(AppConstant.POPULAR_PRODUCTS_URI);
   }
 }
