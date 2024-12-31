e68d9a92dd9e7770dc9f04cb4cf93e84d54ee4fa
Sabiha <ssabiha104@gmail.com>
2024-12-31 06:48:30 -0800

    Add api

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -2,10 +2,11 @@ import 'package:get/get.dart';
 import 'package:shop_app_with_backend/controller/popular_product_controller.dart';
 import 'package:shop_app_with_backend/data/api/api_client.dart';
 import 'package:shop_app_with_backend/data/api/repository/popular_product_repo.dart';
+import 'package:shop_app_with_backend/utils/app_constants.dart';
 
 Future<void> init()async {
   // api client
-Get.lazyPut(()=>ApiClient(appBaseUrl: 'http://mvs.bslmeiyu.com', token: 'token'));
+Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstant.BASE_URL, token: 'token'));
 //repos
 Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
 //controllers
