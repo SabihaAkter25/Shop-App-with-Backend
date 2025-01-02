7d6f360caf8367863e2dc8c58236a3555e106774
Sabiha <ssabiha104@gmail.com>
2025-01-01 22:31:58 -0800

    Show Popular Products

---
+++ lib/data/api/repository/recommended_product_repo.dart
@@ -0,0 +1,13 @@
+import 'package:get/get.dart';
+import 'package:shop_app_with_backend/data/api/api_client.dart';
+import 'package:shop_app_with_backend/utils/app_constants.dart';
+
+class RecommendedProductRepo extends GetxService {
+  final ApiClient apiClient;
+
+  RecommendedProductRepo({required this.apiClient});
+
+  Future<Response> getRecommendedProductList() async {
+    return await apiClient.getData(AppConstant.RECOMMENDED_PRODUCTS_URI);
+  }
+}
