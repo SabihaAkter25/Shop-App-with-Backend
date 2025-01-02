6851cc0f4b569e8b30abdc8de604d97401e5d737
Sabiha <ssabiha104@gmail.com>
2025-01-01 22:53:36 -0800

    Show Recommended Products

--- lib/controller/recommended_product_controller.dart
+++ lib/controller/recommended_product_controller.dart
@@ -17,11 +17,13 @@ class RecommendedProductController extends GetxController {
     Response response = await recommendedProductRepo.getRecommendedProductList();
 
     if(response.statusCode==200){
+      print("got products recommended");
       _recommendedProductList=[];
       _recommendedProductList.addAll(Product.fromJson(response.body).products);
       _isLoaded=true;
       update();
     }else{
+      print("could not get products recommended");
 
     }
 
