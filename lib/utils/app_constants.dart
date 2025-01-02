d7dc4326311b4cf9a8f421b673083915fa7fc196
Sabiha <ssabiha104@gmail.com>
2025-01-01 23:05:53 -0800

    Show Recommended Products

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -5,6 +5,8 @@ class AppConstant{
   static const String BASE_URL ="http://mvs.bslmeiyu.com";
   static const String POPULAR_PRODUCTS_URI ="/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCTS_URI ="/api/v1/products/recommended";
+  static const String UPLOAD_URL ="/uploads/";
+
 
   static const String TOKEN ="DBtoken";
 
