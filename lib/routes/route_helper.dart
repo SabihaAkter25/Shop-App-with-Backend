0f4cf490052eda1fac13dd0aa3636d367c2e5244
Sabiha <ssabiha104@gmail.com>
2025-01-03 22:30:44 -0800

    Add dynamic routes

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -1,7 +1,6 @@
 import 'package:get/get.dart';
 import 'package:shop_app_with_backend/pages/food/popular_food_detail.dart';
 import 'package:shop_app_with_backend/pages/home/main_food_page.dart';
-
 import '../pages/food/recomended_food_detail.dart';
 
 class RouteHelper{
@@ -11,20 +10,22 @@ class RouteHelper{
   static const String recommendedFood ="/recommended-food";
 
   static  String getInitial() =>'$initial';
-  static  String getPopularFood() =>'$popularFood';
+  static  String getPopularFood(int pageId) =>'$popularFood?pageId=$pageId';
   static  String getRecommendedFood() =>'$recommendedFood';
 
 
   static List<GetPage> routes=[
-     GetPage(name: initial, page:()=>MainFoodPage()),
+     GetPage(name: initial, page:()=> MainFoodPage()),
      GetPage(name: popularFood, page:(){
-       return PopularFoodDetail();
+       var pageId = Get.parameters['pageId'];
+       return PopularFoodDetail( pageId: int.parse(pageId!),);
       },
        transition: Transition.fadeIn,
      ),
   GetPage(name: recommendedFood, page:(){
   return RecomendedFoodDetail();
-  },)
+    },
+  )
    ];
 
 }
