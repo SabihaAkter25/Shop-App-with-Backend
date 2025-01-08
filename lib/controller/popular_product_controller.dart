d74cee19438f879fb59679b3c51e684f2b5ae311
Sabiha <ssabiha104@gmail.com>
2025-01-08 00:31:48 -0800

    Adding cart

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -11,7 +11,7 @@ class PopularProductController extends GetxController {
   List<dynamic> get popularProductList => _popularProductList;
   bool _isLoaded = false;
   bool get isLoaded => _isLoaded;
-
+  int _quantity = 0;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
@@ -26,6 +26,14 @@ class PopularProductController extends GetxController {
     }
 
   }
+setQuantity(bool isIncrement){
+    if(isIncrement){
+      _quantity = _quantity+1;
+    }else{
+      _quantity = _quantity-1;
+    }
+}
+
 }
 
 
