7a134d347566ec62c0a3f0585caced830b39a2d1
Sabiha <ssabiha104@gmail.com>
2025-01-10 21:51:18 -0800

    Adding cart

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,6 +1,8 @@
 import 'package:get/get.dart';
+import 'package:shop_app_with_backend/controller/cart_controller.dart';
 import 'package:shop_app_with_backend/controller/popular_product_controller.dart';
 import 'package:shop_app_with_backend/data/api/api_client.dart';
+import 'package:shop_app_with_backend/data/api/repository/cart_repo.dart';
 import 'package:shop_app_with_backend/data/api/repository/popular_product_repo.dart';
 import 'package:shop_app_with_backend/utils/app_constants.dart';
 
@@ -9,14 +11,15 @@ import '../data/api/repository/recommended_product_repo.dart';
 
 Future<void> init()async {
   // api client
-Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstant.BASE_URL, token: 'token'));
+Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstant.BASE_URL, token:'token'));
 //repos
 Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
   Get.lazyPut(()=>RecommendedProductRepo(apiClient: Get.find()));
+  Get.lazyPut(()=>CartRepo());
 
 //controllers
 Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));
   Get.lazyPut(()=>RecommendedProductController(recommendedProductRepo: Get.find()));
-
-}
+Get.lazyPut(()=>CartController(cartRepo: Get.find()));
+ }
 
