df8dbeebe463e9bff4b6e4118981c01bd9da30f0
Sabiha <ssabiha104@gmail.com>
2025-01-10 20:58:43 -0800

    Adding cart

--- lib/controller/popular_product_controller.dart
+++ lib/controller/popular_product_controller.dart
@@ -9,58 +9,70 @@ class PopularProductController extends GetxController {
   PopularProductController({required this.popularProductRepo});
 
   List<dynamic> _popularProductList = [];
+
   List<dynamic> get popularProductList => _popularProductList;
   bool _isLoaded = false;
+
   bool get isLoaded => _isLoaded;
   int _quantity = 0;
-  int get quantity=>_quantity;
+
+  int get quantity => _quantity;
+  int _inCartItems = 0;
+
+  int get inCartItems => _inCartItems + _quantity;
 
   Future<void> getPopularProductList() async {
     Response response = await popularProductRepo.getPopularProductList();
 
-    if(response.statusCode==200){
-      _popularProductList=[];
-      _popularProductList.addAll(Product.fromJson(response.body).products);
-      _isLoaded=true;
+    if (response.statusCode == 200) {
+      _popularProductList = [];
+      _popularProductList.addAll(Product
+          .fromJson(response.body)
+          .products);
+      _isLoaded = true;
       update();
-    }else{
+    } else {
 
     }
-
   }
-setQuantity(bool isIncrement){
-    print("increment"+_quantity.toString());
-    if(isIncrement){
-      _quantity =checkQuantity( _quantity+1);
-    }else{
-      _quantity = checkQuantity( _quantity-1);
+
+  setQuantity(bool isIncrement) {
+    print("increment" + _quantity.toString());
+    if (isIncrement) {
+      _quantity = checkQuantity(_quantity + 1);
+    } else {
+      _quantity = checkQuantity(_quantity - 1);
     }
     update();
-}
-int checkQuantity(int quantity){
-    if(quantity<0){
+  }
+
+  int checkQuantity(int quantity) {
+    if (quantity < 0) {
       Get.snackbar("Item count", "You can't reduce more!",
-      backgroundColor: Colors.tealAccent.shade400,
-      colorText: Colors.white,
+        backgroundColor: Colors.tealAccent.shade400,
+        colorText: Colors.white,
       );
       return 0;
-    }else if(quantity>20){
+    } else if (quantity > 20) {
       Get.snackbar("Item count", "You can't add more!",
         backgroundColor: Colors.tealAccent.shade400,
         colorText: Colors.white,
       );
       return 20;
-    }else{
+    } else {
       return quantity;
     }
-}
-void initProduct(){
-    _quantity =0;
-}
+  }
+
+  void initProduct() {
+    _quantity = 0;
+    _inCartItems = 0;
+    //if exist
+    // get from storage _inCartItems=3
+
+  }
 
 }
-
-
 
 
 
