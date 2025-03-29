3e468743645fc7090ef4c0b025b894a20613a236
Sabiha <ssabiha104@gmail.com>
2025-03-28 22:59:24 -0700

    Adding cart

--- lib/model/cart_model.dart
+++ lib/model/cart_model.dart
@@ -1,21 +1,32 @@
 class CartModel {
-  CartModel({
-    required this.id,
-    required this.name,
-    required this.price,
-    required this.img,
-  });
 
   final int? id;
   final String? name;
   final int? price;
   final String? img;
+  final int? quantity;
+  final bool? isExist;
+  final String? time;
+  CartModel({
+    required this.id,
+    required this.name,
+    required this.price,
+    required this.img,
+    required this.quantity,
+    required this.isExist,
+    required this.time,
+  });
+
+
   factory CartModel.fromJson(Map<String, dynamic> json){
     return CartModel(
       id: json["id"],
       name: json["name"],
       price: json["price"],
       img: json["img"],
+        time:json['time'],
+      isExist:json['isExist'],
+      quantity:json['quantity'],
         );
   }
 }
