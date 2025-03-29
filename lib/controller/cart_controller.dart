1e9e949095ecf76284c10b286cb3f2137d72fcec
Sabiha <ssabiha104@gmail.com>
2025-03-28 22:52:50 -0700

    Adding cart

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,9 +1,11 @@
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:shop_app_with_backend/data/api/repository/cart_repo.dart';
 
+import '../model/cart_model.dart';
+
 class CartController extends GetxController{
  final CartRepo cartRepo;
  CartController({required this.cartRepo});
-
+Map<int, CartModel> items ={};
 
 }
