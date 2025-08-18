86c0575402a1fb5c9fca90fdcace5fb341c41ddb
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:18:22 -0700

    Add dynamic routes

--- lib/routes/route_helper.dart
+++ lib/routes/route_helper.dart
@@ -1,7 +1,7 @@
 import 'package:get/get.dart';
-import 'package:shop_app_with_backend/pages/food/popular_food_detail.dart';
-import 'package:shop_app_with_backend/pages/home/main_food_page.dart';
+import '../pages/food/popular_food_detail.dart';
 import '../pages/food/recomended_food_detail.dart';
+import '../pages/home/main_food_page.dart';
 
 class RouteHelper{
 
@@ -9,25 +9,25 @@ class RouteHelper{
   static const String popularFood ="/popular-food";
   static const String recommendedFood ="/recommended-food";
 
-  static  String getInitial() =>'$initial';
+  static  String getInitial() =>'$initial';                                                   //এখানে / হল root route বা অ্যাপের শুরু পেজের path।
   static  String getPopularFood(int pageId) =>'$popularFood?pageId=$pageId';
   static  String getRecommendedFood(int pageId) =>'$recommendedFood?pageId=$pageId';
 
-
   static List<GetPage> routes=[
-     GetPage(name: initial, page:()=> MainFoodPage()),
-     GetPage(name: popularFood, page:(){
-       var pageId = Get.parameters['pageId'];
-       return PopularFoodDetail( pageId: int.parse(pageId!));
-      },
-       transition: Transition.fadeIn,
-     ),
-  GetPage(name: recommendedFood, page:(){
-    var pageId = Get.parameters['pageId'];
-  return RecommendedFoodDetail(pageId: int.parse(pageId!));
+
+    GetPage(name: initial, page:()=> MainFoodPage()),
+    GetPage(name: popularFood, page:(){
+      var pageId = Get.parameters['pageId'];
+      return PopolarFoodDetail( pageId: int.parse(pageId!));
     },
-    transition: Transition.fadeIn,
-  )
-   ];
+      transition: Transition.fadeIn,
+    ),
+    GetPage(name: recommendedFood, page:(){
+      var pageId = Get.parameters['pageId'];
+      return RecommendedFoodDetail(pageId: int.parse(pageId!));
+    },
+      transition: Transition.fadeIn,
+    )
+  ];
 }
 
