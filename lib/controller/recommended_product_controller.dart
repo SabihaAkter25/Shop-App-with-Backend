6d2ab462ec7c6fc0d1aee606760e055cd0bd82eb
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:22:25 -0700

    Add api

--- lib/controller/recommended_product_controller.dart
+++ lib/controller/recommended_product_controller.dart
@@ -1,35 +1,32 @@
 import 'package:get/get.dart';
-import 'package:shop_app_with_backend/model/product_model.dart';
+
 import '../data/api/repository/recommended_product_repo.dart';
+import '../model/product_model.dart';
 
-
-class RecommendedProductController extends GetxController {
+class RecommendedProductController extends GetxController{
   final RecommendedProductRepo recommendedProductRepo;
+  RecommendedProductController({required this.recommendedProductRepo});
 
-  RecommendedProductController({required this.recommendedProductRepo,} );
+  List<ProductModel> _recommendedProductList=[];
+  List<ProductModel> get recommendedProductList =>_recommendedProductList;
+  bool _isloaded = false;
+  bool get isloaded => _isloaded;
 
-  List<dynamic> _recommendedProductList = [];
-  List<dynamic> get recommendedProductList => _recommendedProductList;
-  bool _isLoaded = false;
-  bool get isLoaded => _isLoaded;
-
-
-  Future<void> getRecommendedProductList() async {
+  Future<void> getRecommendedProductList()async{
     Response response = await recommendedProductRepo.getRecommendedProductList();
 
     if(response.statusCode==200){
+
       _recommendedProductList=[];
       _recommendedProductList.addAll(Product.fromJson(response.body).products);
-      _isLoaded=true;
+      _isloaded = true;
       update();
     }else{
-      print("could not get products recommended");
-    }
+      print("Error fetching products: ${response.statusCode}");
 
+    }
   }
 }
-
-
 
 
 
