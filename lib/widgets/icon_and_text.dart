ca4548e3223998e67bc7d1b40c79052441b262ae
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:23:38 -0700

    Set const text

--- lib/widgets/icon_and_text.dart
+++ lib/widgets/icon_and_text.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/cupertino.dart';
-import 'package:shop_app_with_backend/utils/dimantions.dart';
+import 'package:flutter/material.dart';
 import 'package:shop_app_with_backend/widgets/small_text.dart';
 
-class IconAndText extends StatelessWidget {
-  const IconAndText({super.key, required this.icon, required this.text, required this.iconColor});
-
+class IconTextWidget extends StatelessWidget {
   final IconData icon;
   final String text;
+
   final Color iconColor;
+  final double size;
+
+  const IconTextWidget({super.key,
+    required this.icon,
+    required this.text,
+
+    required this.iconColor, this.size=20,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Icon(
-          icon,
-          color: iconColor, // Apply the iconColor here
-        size: Dimantions.iconSize24,
 
-        ),
-        SizedBox(width: 5),
-        SmallText(text: text),
+        Icon(icon, color: iconColor,size: size,),
+        SizedBox(width:4,),
+        Smalltext(color: Colors.black45, text: text),
+
       ],
     );
   }
