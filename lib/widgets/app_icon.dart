790c9979963babd41be543d0737a388583fea27c
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:24:40 -0700

    Set const text

--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -1,32 +1,38 @@
-import 'package:flutter/cupertino.dart';
-import 'package:shop_app_with_backend/utils/dimantions.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/dimentions.dart';
+
+
 
 class AppIcon extends StatelessWidget {
   final IconData icon;
   final Color backgroundColor;
-  final Color iconcolor;
+  final Color iconColor;
   final double size;
   const AppIcon({super.key,
     required this.icon,
-    this.backgroundColor=const Color(0xFFfcf4e4),
-    this.iconcolor= const Color(0xFF756d54),
-    this.size=40, required color
-  });
+    this.backgroundColor= const Color(0xFFfcf4e4),
+    this.iconColor=const Color(0xFF756d54),
+    this.size=40,
+
+  }
+      );
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: size,
       width: size,
-      decoration: BoxDecoration(
+      height: size,
+      decoration:BoxDecoration(
         borderRadius: BorderRadius.circular(size/2),
-     color:backgroundColor
-      ),
-      child:Icon(
-        icon,
-        color: iconcolor,
-        size:Dimantions.iconSize16,
+        color: backgroundColor,
       ) ,
+      child: Icon(
+        icon,
+        color: iconColor,
+        size:Dimentions.iconSize16,
+      ),
+
     );
   }
 }
