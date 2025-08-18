f10921aceb8ee5b87f6c43ddb30e62b358806953
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:23:12 -0700

    Add api

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,26 +1,30 @@
 import 'package:get/get.dart';
-import 'package:shop_app_with_backend/controller/cart_controller.dart';
-import 'package:shop_app_with_backend/controller/popular_product_controller.dart';
-import 'package:shop_app_with_backend/data/api/api_client.dart';
-import 'package:shop_app_with_backend/data/api/repository/cart_repo.dart';
-import 'package:shop_app_with_backend/data/api/repository/popular_product_repo.dart';
-import 'package:shop_app_with_backend/utils/app_constants.dart';
-
+import '../controller/cart_controller.dart';
+import '../controller/popular_product_controller.dart';
 import '../controller/recommended_product_controller.dart';
+import '../data/api/api_client.dart';
+import '../data/api/repository/cart_repo.dart';
+import '../data/api/repository/popular_product_repo.dart';
 import '../data/api/repository/recommended_product_repo.dart';
+import '../utils/app_constants.dart';
 
-Future<void> init()async {
-  // api client
-Get.lazyPut(()=>ApiClient(appBaseUrl: AppConstant.BASE_URL, token:'token'));
+Future<void>init()async{
+  //api client
+  Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL, token: ''));           //ApiClient class টিকে Get.lazyPut() দিয়ে register করা হয়েছে।
+
+  //lazyPut মানে হলো যখন দরকার হবে তখনই object তৈরি হবে (lazy loading).
+
+  //appBaseUrl: "" এখনো ঠিক করা হয়নি, মানে এটা default বা placeholder URL।
 //repos
-Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));
+  Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));                  //এখানে PopularProductRepo নামে একটা repository তৈরি করা হয়েছে এবং এর মধ্যে ApiClient inject করা হয়েছে Get.find() দিয়ে।
   Get.lazyPut(()=>RecommendedProductRepo(apiClient: Get.find()));
   Get.lazyPut(()=>CartRepo());
+  // Get.find() GetX-এর method যেটা আগে register করা instance কে খুঁজে বের করে দেয় (এই ক্ষেত্রে ApiClient)।
 
-//controllers
-Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));
+//controller
+  Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));     //এখানে PopularProductController register করা হয়েছে এবং তার মধ্যে PopularProductRepo inject করা হয়েছে Get.find() দিয়ে।
   Get.lazyPut(()=>RecommendedProductController(recommendedProductRepo: Get.find()));
-Get.lazyPut(()=>CartController(cartRepo: Get.find()));
+  Get.lazyPut (()=>CartController(cartRepo: Get.find()));
+}
 
- }
-
+// shob controller ekta file e load kora holo
