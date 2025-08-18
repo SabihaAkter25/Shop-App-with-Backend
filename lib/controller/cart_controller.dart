bf7c1d2dc98ff8f34498ced5ca3ed8eb3807c1f7
Sabiha <ssabiha104@gmail.com>
2025-08-17 18:59:40 -0700

    Adding cart

--- lib/controller/cart_controller.dart
+++ lib/controller/cart_controller.dart
@@ -1,25 +1,87 @@
-import 'package:get/get_state_manager/src/simple/get_controllers.dart';
-import 'package:shop_app_with_backend/data/api/repository/cart_repo.dart';
-import 'package:shop_app_with_backend/model/product_model.dart';
-import '../model/cart_model.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../model/cart_model.dart';
+import '../data/api/repository/cart_repo.dart';
+import '../model/product_model.dart';
+
+
 class CartController extends GetxController{
- final CartRepo cartRepo;
- CartController({required this.cartRepo});
-Map<int, CartModel> _items ={};
-void addItem(ProductElement product, int quantity){
-if(quantity>0){
- _items.putIfAbsent(product.id!, () {
-  print("Adding item to the cart "+ product.id!.toString() + "quantity "+quantity.toString());
-  return CartModel(
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      img: product.img,
-      quantity: quantity,
-      isExist: true,
-      time: DateTime.now().toString());
- });
-}
-}
+
+  final CartRepo cartRepo;
+  CartController({required this.cartRepo});
+  Map<int, CartModel> _items={};
+  Map<int, CartModel> get items =>_items;
+  void addItem(ProductModel product, int quantity){
+    var totalQuantity=0;
+    if(_items.containsKey(product.id!)){
+      _items.update(product.id!, (value){
+        totalQuantity = value.quantity!+quantity;
+
+        return CartModel(
+            id:value.id,
+            name:value.name,
+            price:value.price,
+            img:value.img,
+            quantity:value.quantity!+quantity,
+            isExist:true,
+            time:DateTime.now().toString()
+        );
+      }
+      );
+      if(totalQuantity<=0){
+        _items.remove(product.id);
+      }
+    }else{
+      if(quantity>0){
+        _items.putIfAbsent(product.id!,(){
+          return CartModel(
+              id:product.id,
+              name:product.name,
+              price:product.price,
+              img:product.img,
+              quantity:quantity,
+              isExist:true,
+              time:DateTime.now().toString()
+          );
+        }
+        );
+
+      }else{
+        Get.snackbar("Item count", "You must add at least an item",
+          backgroundColor: Colors.tealAccent.shade700,
+          colorText: Colors.white,
+        );
+      }
+    }
+  }
+
+  bool existInCart(ProductModel product){
+    if(_items.containsKey(product.id)){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  int getQuantity(ProductModel product) {
+    var quantity = 0;
+    if (_items.containsKey(product.id)) {
+      _items.forEach((key, value) {
+        if (key == product.id) {
+          quantity = value.quantity!;
+        }
+      });
+    }
+    return quantity;
+  }
+
+  int get totalItems{
+    var totalQuantity=0;
+    _items.forEach((key, value){
+      totalQuantity += value.quantity!;
+
+    });
+    return totalQuantity;
+
+  }
 
 }
--- lib/data/api/api_client.dart
+++ lib/data/api/api_client.dart
@@ -1,26 +1,39 @@
-import 'package:get/get.dart';
-import 'package:shop_app_with_backend/utils/app_constants.dart';
+import 'package:get/get_connect/connect.dart';                             //এটা GetConnect ক্লাস আনে, যা দিয়ে HTTP রিকোয়েস্ট করা যায়।
+import 'package:get/get_state_manager/src/rx_flutter/rx_disposable.dart';
+import '../../utils/app_constants.dart';  // এটা GetxService ক্লাস আনে, যা GetX-এর সার্ভিস হিসেবে ব্যবহার হয় এবং অ্যাপ চালু থাকা পর্যন্ত জীবিত (alive) থাকে।
 
 class ApiClient extends GetConnect implements GetxService{
-  late String token;
-  late String appBaseUrl;
-late Map<String, String> _mainHeaders;
-ApiClient({required this.appBaseUrl, required this.token}){
-  baseUrl = appBaseUrl;
-  timeout = const Duration(seconds: 30);
-  token =AppConstant.TOKEN;
-  _mainHeaders={
-'content_type': 'application/json; charset=UTF-8',
-    'Authorization':'Bearer $token',
-  };
-}
-Future<Response>getData(String uri) async {
-  try{
-Response response=await get(uri);
-return response;
-  }catch(e){
-    return Response(statusCode: 1, statusText: e.toString());
-  }
- }
-}
+  //এখানে ApiClient নামে একটা ক্লাস বানানো হয়েছে,
+  // যেটা GetConnect থেকে সব HTTP ফাংশনগুলো ইনহেরিট করেছে (যেমন: get(), post() ইত্যাদি)।
+  //  আর implements GetxService মানে এটা GetX সার্ভিস হিসেবেও কাজ করবে।
 
+  late String token;                                          //তোমার Authorization টোকেন (Bearer Token) রাখবে, যেটা API এর সাথে নিরাপদে যোগাযোগের জন্য দরকার।
+
+  final String appBaseUrl;                                // API এর মূল URL (যেমন: https://example.com/api)। Constructor থেকে পাঠানো হবে।
+
+  late Map<String,String>_mainHeader;                   //_mainHeader: Header data store korbe — ja prottek API call e lagbe.
+
+  ApiClient({required this.appBaseUrl, this.token=''} ){              //this.appBaseUrl: constructor parameter dhore baseUrl set kora hochche.
+    baseUrl = appBaseUrl;
+    token =AppConstants.TOKEN;
+    timeout = Duration(seconds:30);                        //timeout: 30 second porjonto API response na pele timeout dibe
+
+    _mainHeader=  {                                            //_mainHeader: ekta map create kora hocche:
+      //   Content-type: json data pathacchi.
+      //  Authorization: user-er token add kore authorization header set hocche. (Kintu ekhane ekta bug ache, eta pore bolbo.)
+      'Content-type':'application/json; charset=UTF-8',
+      'Authorization': 'Bearer $token',
+    };
+  }                                                        //async:ফাংশনের ভিতরের কোড asynchronous, মানে এটা অন্য কাজের সাথে পাল্লা দিয়ে চলবে। এর ভিতরে আমরা await ব্যবহার করতে পারি, যেটা async ফাংশন থেকে result না আসা পর্যন্ত অপেক্ষা করে।
+
+  Future<Response> getData(String uri)async{               //Future<Response>:
+    //এই ফাংশন ভবিষ্যতে (async) Response টাইপের একটা ডেটা ফিরিয়ে দেবে।Response হচ্ছে GetX-এর get() মেথড থেকে পাওয়া HTTP রেসপন্স অবজেক্ট।
+    try{
+      //try-catch Block:  এটি ব্যবহার করা হয় error handle করার জন্য, যেন কোনো exception (ভুল) হলে অ্যাপ ক্র্যাশ না করে সুন্দরভাবে একটি রেসপন্স দেয়।
+      Response response = await get(uri,headers: _mainHeader);                 //get(uri): এটা GetX-এর GetConnect থেকে আসে।  এটা uri এ থাকা ঠিকানা অনুযায়ী HTTP GET রিকোয়েস্ট পাঠায়।
+      return response;
+    }catch(e){
+      return Response(statusCode: 1, statusText: e.toString());     //statusCode: 1 মানে:"এই রেসপন্স কোনো HTTP সার্ভার থেকে আসেনি। এটা কোডে exception ধরা পড়ার ফল।"
+    }
+  }
+}
