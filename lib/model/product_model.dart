e47bc2664e969be36c1e9b8f6d00f93c45713c4b
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:20:39 -0700

    Add api

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,72 +1,68 @@
 class Product {
-  Product({
-    required totalSize,
-    required typeId,
-    required offset,
-    required products,
-  }){
-    this._totalSize=totalSize;
-    this._typeId=typeId;
-    this._offset=offset;
-    this._products=products;
+  int? _totalSize;
+  int? _typeId;
+  int? _offset;
+  late List<ProductModel> _products;
+  List<ProductModel> get products=>_products;
+  Product({required totalSize, required typeId, required offset, required products}){
+    this._totalSize= totalSize;
+    this._typeId= typeId;
+    this._offset= offset;
+    this._products= products;
+
   }
 
-   int? _totalSize;
-   int? _typeId;
-   int? _offset;
-  late List<ProductElement> _products;
-  List<ProductElement> get products=>_products;
-
-
-  factory Product.fromJson(Map<String, dynamic> json){
-    return Product(
-      totalSize: json["total_size"],
-      typeId: json["type_id"],
-      offset: json["offset"],
-      products: json["products"] == null ? [] : List<ProductElement>.from(json["products"]!.map((x) => ProductElement.fromJson(x))),
-    );
+  Product.fromJson(Map<String, dynamic> json) {
+    _totalSize = json['total_size'];
+    _typeId = json['type_id'];
+    _offset = json['offset'];
+    if (json['products'] != null) {
+      _products = <ProductModel>[];
+      json['products'].forEach((v) {
+        _products!.add(new ProductModel.fromJson(v));
+      });
+    }
   }
+
+
 
 }
 
-class ProductElement {
-  ProductElement({
-    required this.id,
-    required this.name,
-    required this.description,
-    required this.price,
-    required this.stars,
-    required this.img,
-    required this.location,
-    required this.createdAt,
-    required this.updatedAt,
-    required this.typeId,
-  });
+class ProductModel {
+  int? id;
+  String? name;
+  String? description;
+  int? price;
+  int? stars;
+  String? img;
+  String? location;
+  String? createdAt;
+  String? updatedAt;
+  int? typeId;
 
-  final int? id;
-  final String? name;
-  final String? description;
-  final int? price;
-  final int? stars;
-  final String? img;
-  final String? location;
-  final DateTime? createdAt;
-  final DateTime? updatedAt;
-  final int? typeId;
+  ProductModel(
+      {this.id,
+        this.name,
+        this.description,
+        this.price,
+        this.stars,
+        this.img,
+        this.location,
+        this.createdAt,
+        this.updatedAt,
+        this.typeId});
 
-  factory ProductElement.fromJson(Map<String, dynamic> json){
-    return ProductElement(
-      id: json["id"],
-      name: json["name"],
-      description: json["description"],
-      price: json["price"],
-      stars: json["stars"],
-      img: json["img"],
-      location: json["location"],
-      createdAt: DateTime.tryParse(json["created_at"] ?? ""),
-      updatedAt: DateTime.tryParse(json["updated_at"] ?? ""),
-      typeId: json["type_id"],
-    );
+  ProductModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    description = json['description'];
+    price = json['price'];
+    stars = json['stars'];
+    img = json['img'];
+    location = json['location'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    typeId = json['type_id'];
   }
 
 }
