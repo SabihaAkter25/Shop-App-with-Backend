23fbb54fc7b5b752b7cddc44a4243aaaae80766f
Sabiha <ssabiha104@gmail.com>
2025-08-17 22:25:10 -0700

    Add pagebody

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:shop_app_with_backend/widgets/big_text.dart';
-import 'package:shop_app_with_backend/widgets/small_text.dart';
-import '../../utils/dimantions.dart';
+
+import '../../widgets/big_text.dart';
+import '../../widgets/small_text.dart';
 import 'food_page_body.dart';
 
 class MainFoodPage extends StatefulWidget {
@@ -15,49 +14,44 @@ class MainFoodPage extends StatefulWidget {
 class _MainFoodPageState extends State<MainFoodPage> {
   @override
   Widget build(BuildContext context) {
-    if (kDebugMode) {
-      print(MediaQuery.of(context).size.height.toString());
-    }
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.only(top: Dimantions.height45,bottom: Dimantions.height15),
-            padding: const EdgeInsets.only(left:20,right: 20 ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  children: [
-                    BigText(text: "Bangladesh",color: Colors.tealAccent.shade700,),
-                    Row(
-                      children: [
-                        SmallText(text: "Dhaka",color: Colors.black45,),
-                        const Icon(Icons.arrow_drop_down)
-                      ],
-                    )
-                  ],
-                ),
-                Center(
-                  child: Container(
-                    width: Dimantions.height45,
-                    height: Dimantions.height45,
+
+        body:  Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 40,left: 25,right: 25,bottom: 20),
+
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    children: [
+                      Bigtext(color: Colors.teal, text: "Bangladesh", ),
+                      Row(
+                        children: [
+                          Smalltext(color: Colors.black54, text: "Dhaka"),
+                          const Icon(Icons.arrow_drop_down)
+                        ],
+                      ),
+                    ],
+
+                  ),
+
+                  Container(
+                    width: 45,
+                    height: 45,
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(15),
-                      color:Colors.tealAccent.shade700,
-
+                      color: Colors.cyan,
                     ),
-                    child: Icon(Icons.search,color: Colors.white,size: Dimantions.iconSize24,),
+                    child: const Icon(Icons.search,color: Colors.white,),
                   ),
-                )
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            child: FoodPageBody(),
-          )
-        ],
-      ),
+            const Expanded(child: SingleChildScrollView(child: FoodPageBody(),))
+          ],
+        )
     );
   }
 }
