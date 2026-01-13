378df6234828a0ffc86e8f11e1038468b11aec7a
Sabiha <ssabiha104@gmail.com>
2026-01-13 07:26:20 +0600

    increase quantity from cartpage

--- lib/pages/cart/cart_page.dart
+++ lib/pages/cart/cart_page.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:shop_app_with_backend/controller/cart_controller.dart';
+import 'package:shop_app_with_backend/controller/popular_product_controller.dart';
+import 'package:shop_app_with_backend/model/product_model.dart';
 import 'package:shop_app_with_backend/pages/home/main_food_page.dart';
 import 'package:shop_app_with_backend/utils/app_constants.dart';
 import 'package:shop_app_with_backend/widgets/app_icon.dart';
 import 'package:shop_app_with_backend/widgets/big_text.dart';
 import 'package:shop_app_with_backend/widgets/small_text.dart';
+import '../../controller/recommended_product_controller.dart';
+import '../../routes/route_helper.dart';
 import '../../utils/dimentions.dart';
 
 class CartPage extends StatelessWidget {
@@ -31,7 +35,7 @@ class CartPage extends StatelessWidget {
 
               GestureDetector(
                 onTap:(){
-                  Get.to(()=>MainFoodPage());
+                  Get.toNamed(RouteHelper.getInitial());
                 },
 
                 child: AppIcon(icon: Icons.home_outlined,
@@ -57,9 +61,11 @@ class CartPage extends StatelessWidget {
                 child: MediaQuery.removePadding(
                   removeTop: true,
                   context: context,
-                     child: GetBuilder<CartController>(builder: (cartController){
+                     child: GetBuilder<CartController>(
+                         builder: (cartController){
+                       var _cartList = cartController.getItems;
                        return ListView.builder(
-                           itemCount: cartController.getItems.length,
+                           itemCount: _cartList.length,
                            itemBuilder: (_,index){
                              return Container(
                                width: double.maxFinite,
@@ -67,19 +73,52 @@ class CartPage extends StatelessWidget {
                                // color:Colors.blue,
                                child: Row(
                                  children: [
-                                   Container(
-                                     width: Dimentions.height20*5,
-                                     height: Dimentions.height20*5,
-                                     margin: EdgeInsets.only(bottom: Dimentions.height10),
-                                     decoration: BoxDecoration(
-                                         borderRadius: BorderRadius.circular(Dimentions.radius20),
-                                         image: DecorationImage(
-                                             fit: BoxFit.cover,
-                                             image: NetworkImage(
-                                                 AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!,
-                                             ))
-                                     ),
-                                   ),
+                                  GestureDetector(
+                                   onTap: () {
+                                 // 1) Cart থেকে যেই product এ ক্লিক করা হয়েছে, তার ID নাও
+                                 int productId = _cartList[index].product.id!;
+
+                             // 2) Popular এবং Recommended controller নাও (GetX থেকে)
+                             var popularController = Get.find<PopularProductController>();
+                             var recommendedController = Get.find<RecommendedProductController>();
+
+                             // 3) Popular list এ খুঁজে দেখো এই product আছে কিনা
+                             int popularIndex = popularController.popularProductList
+                                 .indexWhere((p) => p.id == productId);
+
+                             if (popularIndex >= 0) {
+                               // যদি popular এ পাওয়া যায় → popular detail page এ যাও
+                               Get.toNamed(RouteHelper.getPopularFood(popularIndex, "cart"));
+                             } else {
+                               // 4) না হলে recommended list এ খুঁজো
+                               int recommendedIndex = recommendedController.recommendedProductList
+                                   .indexWhere((p) => p.id == productId);
+
+                               if (recommendedIndex >= 0) {
+                                 // যদি recommended এ পাওয়া যায় → recommended detail page এ যাও
+                                 Get.toNamed(RouteHelper.getRecommendedFood(recommendedIndex, "cart"));
+                               } else {
+                                 // 5) কোথাও না পেলে error দেখাও
+                                 Get.snackbar("Error", "Product not found");
+                               }
+                             }
+                           },
+
+
+                             child: Container(
+                                    width: Dimentions.height20*5,
+                               height: Dimentions.height20*5,
+                               margin: EdgeInsets.only(bottom: Dimentions.height10),
+                               decoration: BoxDecoration(
+                                   borderRadius: BorderRadius.circular(Dimentions.radius20),
+                                   image: DecorationImage(
+                                       fit: BoxFit.cover,
+                                       image: NetworkImage(
+                                         AppConstants.BASE_URL+AppConstants.UPLOAD_URL+cartController.getItems[index].img!,
+                                       ))
+                               ),
+                             ),
+                                  ),
                                    SizedBox(width:Dimentions.width10 ,),
                                    Expanded(
                                        child: SizedBox(
@@ -105,17 +144,17 @@ class CartPage extends StatelessWidget {
                                                      children: [
                                                        GestureDetector(
                                                            onTap: (){
-                                                             //   popularProduct.setQuantity(false);
+                                                             cartController.addItem(_cartList[index].product, -1);
                                                            },
                                                            child: Icon(Icons.remove,color: Colors.black,)),
                                                        SizedBox(width: Dimentions.width10,),
-                                                       Bigtext(text:"0",
+                                                       Bigtext(text:_cartList[index].quantity.toString(),
                                                          //  text: popularProduct.inCartItems.toString()
                                                        ),
                                                        SizedBox(width: Dimentions.width10,),
                                                        GestureDetector(
                                                            onTap: (){
-                                                             //   popularProduct.setQuantity(true);
+                                                             cartController.addItem(_cartList[index].product, 1);
                                                            },
                                                            child: Icon(Icons.add,color: Colors.black,)),                  ],
                                                    ),
