ac6ad1aae49fd425b27330d614801d49bc7f86ae
Sabiha <ssabiha104@gmail.com>
2026-01-16 08:18:36 +0600

    add animation in splashscreen

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:shop_app_with_backend/routes/route_helper.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -7,9 +11,34 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>with TickerProviderStateMixin{
+  late Animation<double> animation;
+  late AnimationController controller;
+  @override
+  void initState(){
+    super.initState();
+    controller= AnimationController(
+        vsync: this,
+        duration:const Duration(seconds:2))..forward();
+    animation = CurvedAnimation(
+        parent: controller,
+        curve: Curves.linear);
+  }
+  Timer(){
+    const Duration(seconds: 3);
+    ()=>Get.offNamed(RouteHelper.getInitial());
+  }
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ScaleTransition(scale: animation,
+          child: Center(child: Image.asset( "assets/images/Logo.jpg",width: 290,))),
+        ],
+      ),
+    );
   }
 }
