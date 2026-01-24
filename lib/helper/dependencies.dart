0f45e611a25b4d78a0d41d021f6b6e9c9e12b531
Sabiha <ssabiha104@gmail.com>
2026-01-25 02:48:45 +0600

    save cart history in sharedPreference

--- lib/helper/dependencies.dart
+++ lib/helper/dependencies.dart
@@ -1,4 +1,5 @@
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../controller/cart_controller.dart';
 import '../controller/popular_product_controller.dart';
 import '../controller/recommended_product_controller.dart';
@@ -9,6 +10,11 @@ import '../data/api/repository/recommended_product_repo.dart';
 import '../utils/app_constants.dart';
 
 Future<void>init()async{
+
+  final sharedPreferences  = await SharedPreferences.getInstance();
+
+  Get.lazyPut(()=>sharedPreferences);
+
   //api client
   Get.lazyPut(() => ApiClient(appBaseUrl: AppConstants.BASE_URL, token: ''));           //ApiClient class টিকে Get.lazyPut() দিয়ে register করা হয়েছে।
 
@@ -18,11 +24,17 @@ Future<void>init()async{
 //repos
   Get.lazyPut(()=>PopularProductRepo(apiClient: Get.find()));                  //এখানে PopularProductRepo নামে একটা repository তৈরি করা হয়েছে এবং এর মধ্যে ApiClient inject করা হয়েছে Get.find() দিয়ে।
   Get.lazyPut(()=>RecommendedProductRepo(apiClient: Get.find()));
-  Get.lazyPut(()=>CartRepo());
+  Get.lazyPut(()=>CartRepo(sharedPreferences: Get.find()));
   // Get.find() GetX-এর method যেটা আগে register করা instance কে খুঁজে বের করে দেয় (এই ক্ষেত্রে ApiClient)।
 
 //controller
-  Get.lazyPut(()=>PopularProductController(popularProductRepo: Get.find()));     //এখানে PopularProductController register করা হয়েছে এবং তার মধ্যে PopularProductRepo inject করা হয়েছে Get.find() দিয়ে।
+  Get.lazyPut<PopularProductController>(
+        () => PopularProductController(
+      popularProductRepo: Get.find(),
+    ),
+
+  );
+     //এখানে PopularProductController register করা হয়েছে এবং তার মধ্যে PopularProductRepo inject করা হয়েছে Get.find() দিয়ে।
   Get.lazyPut(()=>RecommendedProductController(recommendedProductRepo: Get.find()));
   Get.lazyPut (()=>CartController(cartRepo: Get.find()));
 }
