1b22cd80334e0fbefda9cfa3ae0a0776db1bb997
Sabiha <ssabiha104@gmail.com>
2026-04-11 07:28:32 +0600

    add splash screen

--- lib/pages/splash/splash_screen.dart
+++ lib/pages/splash/splash_screen.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
 import 'package:shop_app_with_backend/routes/route_helper.dart';
-
+import 'package:shop_app_with_backend/utils/dimentions.dart';
 import '../../controller/popular_product_controller.dart';
 import '../../controller/recommended_product_controller.dart';
 
@@ -14,35 +12,44 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>with TickerProviderStateMixin{
-  _loadResources(){
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _loadResources();
+
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+
+    animation = CurvedAnimation(
+      parent: controller,
+      curve: Curves.linear,
+    );
+
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.offNamed(RouteHelper.getInitial());
+    });
+  }
+
+  void _loadResources() {
     Get.find<PopularProductController>().getPopularProductList();
     Get.find<RecommendedProductController>().getRecommendedProductList();
   }
 
-  late Animation<double> animation;
-  late AnimationController controller;
   @override
-  void initState(){
-    _loadResources();
-    super.initState();
-    controller= AnimationController(
-        vsync: this,
-        duration:const Duration(seconds:2))..forward();
-    animation = CurvedAnimation(
-        parent: controller,
-        curve: Curves.linear);
-    Future.delayed(const Duration(seconds: 3),(){
-      Get.offNamed(RouteHelper.getInitial());
-    });
-    @override
-    void dispose() {
-      controller.dispose();
-      super.dispose();
-    }
-
-
+  void dispose() {
+    controller.dispose();   // ✅ Proper dispose
+    super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -50,8 +57,15 @@ class _SplashScreenState extends State<SplashScreen>with TickerProviderStateMixi
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          ScaleTransition(scale: animation,
-          child: Center(child: Image.asset( "assets/images/Logo.jpg",width: 290,))),
+          ScaleTransition(
+            scale: animation,
+            child: Center(
+              child: Image.asset(
+                "assets/images/Logo.jpg",
+                width: Dimentions.splashImg,
+              ),
+            ),
+          ),
         ],
       ),
     );
