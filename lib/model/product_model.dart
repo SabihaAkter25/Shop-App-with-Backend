6fd318fa0fd0986b07b61e021c3f8d60a17c1813
Sabiha <ssabiha104@gmail.com>
2026-04-11 07:29:53 +0600

    changed api key

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -13,9 +13,11 @@ class Product {
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      totalSize: json['total_size'],
-      typeId: json['type_id'],
-      offset: json['offset'],
+      totalSize: json['total'],
+      typeId: null,
+      offset: json['skip'],
+
+
       products: json['products'] != null
           ? List<ProductModel>.from(
           json['products'].map((x) => ProductModel.fromJson(x)))
@@ -28,38 +30,66 @@ class ProductModel {
   int? id;
   String? name;
   String? description;
-  int? price;
-  int? stars;
-  String? img;
+  double? price;
+  double? stars;
+  String? img;// thumbnail
+  String? thumbnail;
+  List<String>? images;     // extra images
   String? location;
   String? createdAt;
   String? updatedAt;
   int? typeId;
 
+  ProductModel({
+    this.id,
+    this.name,
+    this.description,
+    this.price,
+    this.stars,
+    this.img,
+    this.images,
+    this.location,
+    this.createdAt,
+    this.updatedAt,
+    this.typeId,
+    this.thumbnail
+  });
+
   ProductModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    name = json['name'];
+    name = json['title'];
     description = json['description'];
-    price = json['price'];
-    stars = json['stars'];
-    img = json['img'];
-    location = json['location'];
-    createdAt = json['created_at'];
-    updatedAt = json['updated_at'];
-    typeId = json['type_id'];
+
+    // ✅ safe conversion
+    price = (json['price'] as num?)?.toDouble();
+    stars = (json['rating'] as num?)?.toDouble();
+
+    // 🔥 IMPORTANT (thumbnail)
+
+    thumbnail = json['thumbnail'];
+    // 🔥 images list
+    if (json['images'] != null) {
+      images = List<String>.from(json['images']);
+    }
+    location = null;
+    createdAt = null;
+    updatedAt = null;
+    typeId = null;
   }
 
-  Map<String,dynamic>tojson() {
-    return{
-      "id":id,
-      "name":name,
-      "price":price,
-      "img":img,
-      "location":location,
-      "createdAt":createdAt,
-      "updatedAt":updatedAt,
-      "typeId":typeId
-
+  Map<String, dynamic> toJson() {
+    return {
+      "id": id,
+      "title": name,
+      "description": description,
+      "price": price,
+      "rating": stars,
+      "thumbnail": thumbnail,
+      "images": images,
+      "location": location,
+      "createdAt": createdAt,
+      "updatedAt": updatedAt,
+      "typeId": typeId,
     };
   }
 }
